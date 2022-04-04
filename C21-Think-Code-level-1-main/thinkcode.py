a7d40e07b63bdee3832e29a9d030e730205ce105
HomelessGentleman <91670551+HomelessGentleman@users.noreply.github.com>
2022-04-04 12:49:17 +0200

    eindopdracht thinkcode

---
+++ C21-Think-Code-level-1-main/thinkcode.py
@@ -0,0 +1,14 @@
+print("Hi choose a number between 1 and 2")
+usertext = input("Choose your number: ")
+print("Nice to meet you number", usertext)
+
+print("I finally understand python!")
+answer = input("How about you?: ")
+
+if answer == "its okay":
+    print("That's good to hear!")
+else:
+    print("Oh no! That makes me sad!")
+
+print("Thanks for using my program!")
+#einde
